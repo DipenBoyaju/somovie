4eb06ac5608ebc440df487e56aa1e49844231d9f
DipenBoyaju <78298784+DipenBoyaju@users.noreply.github.com>
2024-07-06 14:46:15 +0545

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ const App = () => {
       element: <RootLayout />,
       children: [
         { index: true, element: <Home /> },
-        // { path: 'topimdb', element: <Movies /> },
+        { path: 'movies', element: <Movies /> },
         // { path: 'topimdb', element: <TvShows /> },
         { path: 'topimdb', element: <TopImdb /> },
         { path: 'movies/:id', element: <MovieDetail /> },
